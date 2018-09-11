4034a5d2280c8604a43be4563dcf26cf3747d1dc
jonathan <jonathansedrez@gmail.com>
2018-09-10 21:29:18 -0300

    adding loading spinner

--- src/components/Loading/styled.js
+++ src/components/Loading/styled.js
@@ -9,11 +9,12 @@ const LoadingSpinner = styled.div`
   position: relative;
   top: calc(50% - 35px);
   left: calc(50% - 35px);
-  border: 5px solid #f3f3f3;
+  
   border-radius: 50%;
-  border-top: 5px solid #3498db;
+  border-top: 3px solid #f46e42;
+  border-bottom: 3px solid #f46e42;
   width: 70px;
   height: 70px;
-  animation: ${spin} 2s linear infinite;
+  animation: ${spin} 1s linear infinite;
 `
 export default LoadingSpinner
