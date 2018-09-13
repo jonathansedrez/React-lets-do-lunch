f83683346db6d10a2440510dfaaf2352f51e6edf
jonathan <jonathansedrez@gmail.com>
2018-09-13 01:06:15 -0300

    config PWA

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './globalStyles'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log(`Service Worker registered! Scope: ${registration.scope}`);
+      })
+      .catch(err => {
+        console.log(`Service Worker registration failed: ${err}`);
+      });
+  });
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "build/",
+  "globPatterns": [
+    "**/*.{json,html,css,js,svg}"
+  ],
+  "swDest": "build/sw.js"
+};
